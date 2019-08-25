c875d6c01a4c88134e2dc874f76c4f1de8b72790
zhouchenlu <969953859@qq.com>
2019-08-25 11:49:31 +0800

    add readme

--- src/main/java/zhou/hardcat/communtiy/controller/Hello.java
+++ src/main/java/zhou/hardcat/communtiy/controller/Hello.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Hello {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(name="name")String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
