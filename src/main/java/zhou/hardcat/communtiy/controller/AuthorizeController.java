21f43b21616cefe51d9bf54cefaf78b84123ab8c
zhouchenlu <969953859@qq.com>
2019-08-25 22:30:59 +0800

    extract github config from code to properties

---
+++ src/main/java/zhou/hardcat/communtiy/controller/AuthorizeController.java
@@ -0,0 +1,37 @@
+package zhou.hardcat.communtiy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import zhou.hardcat.communtiy.datatransferobject.AccessTokenDTO;
+import zhou.hardcat.communtiy.datatransferobject.GithubUser;
+import zhou.hardcat.communtiy.provider.GithubProvider;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private  String clientId;
+    @Value("${github.client.secret}")
+    private  String secret;
+    @Value("${github.redirect.uri}")
+    private  String redirectUri;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(secret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+
+}
---
+++ src/main/java/zhou/hardcat/communtiy/provider/GithubProvider.java
@@ -0,0 +1,52 @@
+package zhou.hardcat.communtiy.provider;
+
+import com.alibaba.fastjson.JSON;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+import zhou.hardcat.communtiy.datatransferobject.AccessTokenDTO;
+import zhou.hardcat.communtiy.datatransferobject.GithubUser;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    //调用GitHub  的 Api accsesstoken
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mt
+                = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mt, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            System.out.println("GithubProvider---->"+string);
+            String[] split = string.split("&");
+            String[] split1 = split[0].split("=");
+            return split1[1];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+
+        try (
+                Response response = client.newCall(request).execute()) {
+            String responseBody = response.body().string();
+            GithubUser githubUser = JSON.parseObject(responseBody, GithubUser.class);
+            return githubUser;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
