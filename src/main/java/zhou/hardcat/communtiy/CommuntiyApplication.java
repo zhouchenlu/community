1b5f15b407458ade1bccff112408ee14d9a60350
zhouchenlu <969953859@qq.com>
2019-08-25 11:37:46 +0800

    init repo

---
+++ src/main/java/zhou/hardcat/communtiy/CommuntiyApplication.java
@@ -0,0 +1,13 @@
+package zhou.hardcat.communtiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuntiyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommuntiyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zhou/hardcat/communtiy/CommuntiyApplicationTests.java
@@ -0,0 +1,16 @@
+package zhou.hardcat.communtiy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommuntiyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
